dc4ad6f4716cfb3bb6d5d327f843784501dba3ca
tiktoksixseven676767-del <tiktoksixseven676767@gmail.com>
2026-05-31 18:21:04 +0200

    Update brrbrrpatapim.py

--- brrbrrpatapim.py
+++ brrbrrpatapim.py
@@ -197,7 +197,8 @@ VIVA IL DUCE 卐
     print(menu_color + " [11] ❌ txt")
     print(menu_color + " [12] ❌ txt")
     print(menu_color + " [13] ❌ txt")
-    print(menu_color + " [14] ❌ ESCI BRUTTO GAY") print("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+    print(menu_color + " [14] ❌ ESCI BRUTTO GAY")
+ print("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
     choice = input(Fore.CYAN + "\n📥 SCEGLI UN OPZIONE: ").strip()
 
     if choice in ["1", "2"]:
